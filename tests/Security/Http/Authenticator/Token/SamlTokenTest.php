caec5a0ee28857d044d9cd63467ae2c11b2e5082
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-04-24 14:47:14 +0000

    Fix php-stan SAMLTokenTest.php

--- tests/Security/Http/Authenticator/Token/SamlTokenTest.php
+++ tests/Security/Http/Authenticator/Token/SamlTokenTest.php
@@ -17,6 +17,9 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(SamlToken::class)]
 final class SamlTokenTest extends TestCase
 {
+    /**
+     * @return iterable<string, array{attributes: array<string, mixed>}>
+     */
     public static function provideTokenCases(): iterable
     {
         yield 'Empty attributes' => [
@@ -31,6 +34,9 @@ final class SamlTokenTest extends TestCase
         ];
     }
 
+    /**
+     * @param array<string, mixed> $attributes
+     */
     #[DataProvider('provideTokenCases')]
     public function testToken(array $attributes): void
     {
